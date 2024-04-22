10cf8c5a1d055686e141baa7d08bca86ae83aa95
Sumit Gohil <153794386+sumitFlutter@users.noreply.github.com>
2024-04-22 17:23:43 +0530

    Update details_screen.dart

--- lib/screen/view/details/details_screen.dart
+++ lib/screen/view/details/details_screen.dart
@@ -19,53 +19,55 @@ class _DetailsScreenState extends State<DetailsScreen> {
       appBar: AppBar(title: Text(countryModel.nameModel!.official!),),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Hero(
-              tag: "enter",
-              child: Container(height: MediaQuery.sizeOf(context).height*0.30,
-              child: Image.network(countryModel.flagModel!.png!,fit: BoxFit.cover,),),
-            ),
-            SizedBox(height: 20,),
-            Text(countryModel!.nameModel!.official!,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
-            SizedBox(height: 10,),
-            Text(countryModel!.nameModel!.common!,style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),),
-            SizedBox(height: 10,),
-            countryModel!.independent!?const Text("Independent Country",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)): Text("Dependent Country",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-            SizedBox(height: 10,),
-            const Text("Continent:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-            SizedBox(height: 10,),
-            SizedBox(
-              height: 70,
-              child: ListView.builder(itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(child: Text(countryModel!.continent![index],style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),)),
-                );
-              },itemCount: countryModel!.continent!.length,),
-            ),
-            const Text("Timezone:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-            SizedBox(height: 10,),
-            SizedBox(
-              height: 70,
-              child: ListView.builder(itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(child: Text(countryModel!.timeZone![index],style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),)),
-                );
-              },itemCount: countryModel!.timeZone!.length,),
-            ),
-            SizedBox(height: 5,),
-            Text("Member of United Nations?",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            countryModel.unMember!?Text("Yes!",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17),):Text("No!",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17),),
-            SizedBox(height: 15,),
-            BottomAppBar(child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.favorite),
-              ],
-            ),)
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Hero(
+                tag: "enter",
+                child: Container(height: MediaQuery.sizeOf(context).height*0.30,
+                child: Image.network(countryModel.flagModel!.png!,fit: BoxFit.cover,),),
+              ),
+              SizedBox(height: 20,),
+              Text(countryModel!.nameModel!.official!,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+              SizedBox(height: 10,),
+              Text(countryModel!.nameModel!.common!,style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),),
+              SizedBox(height: 10,),
+              countryModel!.independent!?const Text("Independent Country",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)): Text("Dependent Country",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+              SizedBox(height: 10,),
+              const Text("Continent:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 70,
+                child: ListView.builder(itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(child: Text(countryModel!.continent![index],style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),)),
+                  );
+                },itemCount: countryModel!.continent!.length,),
+              ),
+              const Text("Timezone:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 70,
+                child: ListView.builder(itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(child: Text(countryModel!.timeZone![index],style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),)),
+                  );
+                },itemCount: countryModel!.timeZone!.length,),
+              ),
+              SizedBox(height: 5,),
+              Text("Member of United Nations?",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              countryModel.unMember!?Text("Yes!",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17),):Text("No!",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17),),
+              SizedBox(height: 15,),
+              BottomAppBar(child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite),
+                ],
+              ),)
+            ],
+          ),
         ),
       ),
     ));
